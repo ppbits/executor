608e8713fb8a4d96f14eb81a2972fb5a696bf9f5
Erik Hollensbe <erik+github@hollensbe.org>
2016-03-23 04:45:10 -0700

    First implementation of /x/net/context capable executor + README
    Signed-off-by: Erik Hollensbe <erik+github@hollensbe.org>

--- executor_test.go
+++ executor_test.go
@@ -9,6 +9,10 @@ import (
 	. "testing"
 	"time"
 
+	"github.com/Sirupsen/logrus"
+
+	"golang.org/x/net/context"
+
 	. "gopkg.in/check.v1"
 )
 
@@ -19,6 +23,7 @@ var cmd = []string{"/bin/sleep", "200000000"}
 var _ = Suite(&execSuite{})
 
 func TestExec(t *T) {
+	logrus.SetLevel(logrus.DebugLevel)
 	TestingT(t)
 }
 
@@ -28,9 +33,7 @@ func (es *execSuite) TestProperties(c *C) {
 	c.Assert(e, NotNil)
 	c.Assert(mycmd, DeepEquals, e.command)
 	c.Assert(e.LogInterval, Equals, 1*time.Minute)
-	c.Assert(e.TerminateChan, NotNil)
 	c.Assert(e.Stdin, IsNil)
-	c.Assert(e.timeout, Equals, time.Duration(0))
 }
 
 func (es *execSuite) TestStartWait(c *C) {
@@ -41,8 +44,8 @@ func (es *execSuite) TestStartWait(c *C) {
 	c.Assert(e.command.Process, NotNil)
 	// the sleep requires we signal the process since it'll sleepp forever.
 	c.Assert(e.command.Process.Signal(syscall.SIGTERM), IsNil)
-	er, err := e.Wait()
-	c.Assert(err, IsNil)
+	er, err := e.Wait(context.Background())
+	c.Assert(err, NotNil)
 	c.Assert(er, NotNil)
 	c.Assert(er.ExitStatus, Equals, uint32(15))
 	c.Assert(er.Runtime, Not(Equals), time.Duration(0))
@@ -50,26 +53,24 @@ func (es *execSuite) TestStartWait(c *C) {
 
 func (es *execSuite) TestStdio(c *C) {
 	cmd := exec.Command("/bin/echo", "yes")
-	e := New(cmd)
+	e := NewCapture(cmd)
 	c.Assert(e.Start(), IsNil)
 	time.Sleep(100 * time.Millisecond) // let it run a bit
-	// yes runs eternally, so kill it manually.
-	c.Assert(e.command.Process.Signal(syscall.SIGTERM), IsNil)
-	er, _ := e.Wait()
+	er, _ := e.Wait(context.Background())
 	c.Assert(er, NotNil)
 	c.Assert(er.Stdout, Matches, "yes\n")
 	cmd = exec.Command("sh", "-c", "echo yes 1>&2")
-	e = New(cmd)
+	e = NewCapture(cmd)
 	c.Assert(e.Start(), IsNil)
 	time.Sleep(100 * time.Millisecond) // let it run a bit
 	// yes runs eternally, so kill it manually.
 	c.Assert(e.command.Process.Signal(syscall.SIGTERM), IsNil)
-	er, _ = e.Wait()
+	er, _ = e.Wait(context.Background())
 	c.Assert(er, NotNil)
 	c.Assert(er.Stderr, Matches, "yes\n")
 
 	cmd = exec.Command("/bin/echo", "yes")
-	e = New(cmd)
+	e = NewIO(cmd)
 	c.Assert(e.Start(), IsNil)
 
 	buf := new(bytes.Buffer)
@@ -77,7 +78,7 @@ func (es *execSuite) TestStdio(c *C) {
 	c.Assert(buf.String(), Equals, "yes\n")
 
 	cmd = exec.Command("sh", "-c", "echo yes 1>&2")
-	e = New(cmd)
+	e = NewIO(cmd)
 	c.Assert(e.Start(), IsNil)
 
 	buf = new(bytes.Buffer)
@@ -85,10 +86,10 @@ func (es *execSuite) TestStdio(c *C) {
 	c.Assert(buf.String(), Equals, "yes\n")
 
 	cmd = exec.Command("cat")
-	e = New(cmd)
+	e = NewCapture(cmd)
 	e.Stdin = bytes.NewBufferString("foo")
 	c.Assert(e.Start(), IsNil)
-	er, err := e.Wait()
+	er, err := e.Wait(context.Background())
 	c.Assert(err, IsNil)
 	c.Assert(er.Stdout, Equals, "foo")
 }
@@ -101,19 +102,18 @@ func (es *execSuite) TestTimeout(c *C) {
 	}
 
 	e.LogFunc = loggerFunc
-	e.SetTimeout(1 * time.Second)
 	c.Assert(e.Start(), IsNil)
-	time.Sleep(2 * time.Second)
-	c.Assert(results[0], Equals, "Command %v terminated due to timeout. It may not have finished!")
-	er, err := e.Wait()
-	c.Assert(err, IsNil)
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+	er, err := e.Wait(ctx)
+	c.Assert(results[0], Equals, "Command %v terminated due to timeout or cancellation. It may not have finished!")
+	c.Assert(err, NotNil)
 	c.Assert(er.ExitStatus, Not(Equals), 0)
 
 	e = New(exec.Command("/bin/sleep", "2"))
+	e.LogFunc = logrus.Infof
 	results = []string{}
-	e.LogFunc = loggerFunc
-	e.SetTimeout(4 * time.Second)
-	er, err = e.Run()
+	ctx, _ = context.WithTimeout(context.Background(), 4*time.Second)
+	er, err = e.Run(ctx)
 	c.Assert(err, IsNil)
 	c.Assert(er.ExitStatus, Equals, uint32(0))
 	time.Sleep(2 * time.Second)
@@ -133,7 +133,7 @@ func (es *execSuite) TestLogger(c *C) {
 	e.LogInterval = 1 * time.Second
 	c.Assert(e.Start(), IsNil)
 	time.Sleep(2 * time.Second)
-	er, _ := e.Wait()
+	er, _ := e.Wait(context.Background())
 	c.Assert(er.ExitStatus, Equals, uint32(0))
 	c.Assert(er.Runtime > 2*time.Second, Equals, true)
 	// sometimes (depending on how long it takes to `cmd.Start()` launch) the
@@ -149,7 +149,35 @@ func (es *execSuite) TestString(c *C) {
 	c.Assert(e.PID(), Not(Equals), uint32(0))
 	c.Assert(e.String(), Equals, fmt.Sprintf("[/bin/sleep 200000000] (/bin/sleep) (pid: %v)", e.PID()))
 	c.Assert(e.command.Process.Signal(syscall.SIGTERM), IsNil)
-	er, _ := e.Wait()
+	er, _ := e.Wait(context.Background())
 	c.Assert(er, NotNil)
 	c.Assert(er.ExitStatus, Equals, uint32(15))
+}
+
+func (es *execSuite) TestCancel(c *C) {
+	e := New(exec.Command(cmd[0], cmd[1:]...))
+	results := []string{}
+	loggerFunc := func(s string, args ...interface{}) {
+		results = append(results, s)
+	}
+
+	e.LogFunc = loggerFunc
+	c.Assert(e.Start(), IsNil)
+	ctx, cancel := context.WithCancel(context.Background())
+	erChan := make(chan *ExecResult, 1)
+	errChan := make(chan error, 1)
+	go func() {
+		er, err := e.Wait(ctx)
+		erChan <- er
+		errChan <- err
+	}()
+
+	cancel()
+	er := <-erChan
+	err := <-errChan
+	c.Assert(er, NotNil)
+	c.Assert(er.executor, NotNil)
+	c.Assert(err, NotNil)
+	c.Assert(err, Equals, context.Canceled)
+	c.Assert(results[0], Equals, "Command %v terminated due to timeout or cancellation. It may not have finished!")
 }
